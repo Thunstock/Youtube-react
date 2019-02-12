6fc6a6bc85b2708982362702ed8166d955f89f3c
Tyler Hunstock <Thunstock17@gmail.com>
2019-02-12 12:14:03 -0800

    add react router & tests & video component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
